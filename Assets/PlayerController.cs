1aaeac709fb5c236cc1ecdd520c9bc807132362b
Zeerak Baig <zeerak@bbd.co.za>
2023-08-24 18:00:59 +0200

    Added animations for the player

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -10,19 +10,18 @@ public class PlayerController : MonoBehaviour
     public float collisionOffset = 0.05f;
     public ContactFilter2D movementFilter;
     Vector2 movementInput;
+    SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
+    Animator animator;
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
-
-    // Update is called once per frame
-    /*    void Update()
-        {
-        }*/
 
     private void FixedUpdate()
     {
@@ -32,25 +31,41 @@ public class PlayerController : MonoBehaviour
             if (!success)
             {
                 success = TryMove(new Vector2(movementInput.x, 0));
-
-                if (!success)
-                {
-                    success = TryMove(new Vector2(0, movementInput.y));
-                }
             }
+            
+            if (!success)
+            {
+                success = TryMove(new Vector2(0, movementInput.y));
+            }
+
+            animator.SetBool("isMoving", success);
+        }else{
+            animator.SetBool("isMoving", false);
         }
+
+        //set the direction of sprite to movement direction
+        if(movementInput.x < 0){
+            spriteRenderer.flipX = true;
+        }else if(movementInput.x > 0){
+            spriteRenderer.flipX = false;
+        }
+        
     }
 
     private bool TryMove(Vector2 direction)
     {
-        int count = rb.Cast(direction, movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime + collisionOffset);
-        if (count == 0)
-        {
-            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
-            return true;
-        }
-        else
-        {
+        if(direction != Vector2.zero){
+            int count = rb.Cast(direction, movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime + collisionOffset);
+            if (count == 0)
+            {
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }else{
             return false;
         }
     }
