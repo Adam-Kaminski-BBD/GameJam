61556a2350b53aaf718162f2fa74cd830ac5a059
Nicholas Joannou <nicholasj@bbd.co.za>
2023-08-24 20:34:20 +0200

    Added health stuff

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,22 +7,48 @@ using System.ComponentModel;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 1f;
-    public float collisionOffset = 0.05f;
+    public float collisionOffset = 0.0f;
     public ContactFilter2D movementFilter;
     Vector2 movementInput;
     Rigidbody2D rb;
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
+    public int maxHealth = 100;
+    public int currentHealth;
+    public HealthBar healthBar;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
+		healthBar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
-    /*    void Update()
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-        }*/
+            TakeDamage(20);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "CollisionObjects")
+        {
+            TakeDamage(5);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+    }
 
     private void FixedUpdate()
     {
