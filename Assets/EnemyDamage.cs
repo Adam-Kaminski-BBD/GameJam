a700c8342845493ec394fb714e5a3e21c3df5471
ZeerakBaig <zmbaig98@gmail.com>
2023-09-02 19:19:04 +0200

    added enemy death animation

--- Assets/EnemyDamage.cs
+++ Assets/EnemyDamage.cs
@@ -9,10 +9,14 @@ public class EnemyDamage : MonoBehaviour
     public int currentHealth;
     public HealthBar healthBar;
 
+    //enemy death animation
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
     }
 
     void OnTriggerEnter2D(Collider2D col)
@@ -33,14 +37,43 @@ public class EnemyDamage : MonoBehaviour
     void TakeDamage(int damage)
     {
         currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Die(); // Call the Die method when health reaches zero.
+        }
+    }
+
+    void Die()
+    {
+        // Trigger the "DieTrigger" parameter in the Animator to play the death animation.
+        animator.SetTrigger("DeathTrigger");
+        StartCoroutine(DestroyAfterAnimation());
+
+
     }
 
     // Update is called once per frame
-    void Update()
+   /* void Update()
     {
-        if (currentHealth == 0)
+        if (currentHealth <= 0)
         {
+            //animator.SetTrigger("DeathTrigger");
             Destroy(gameObject);
         }
+    }*/
+
+    // Method to destroy the enemy after the death animation has finished (if needed).
+    void DestroyEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+    private IEnumerator DestroyAfterAnimation()
+    {
+        yield return new WaitForSeconds(0.6f);
+
+        // Destroy the GameObject
+        Destroy(gameObject);
     }
 }
